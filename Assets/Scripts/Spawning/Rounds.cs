439f59ec1532270e6f06e2d3968668a67f5348a5
Austin Offill <austin1998@tamu.edu>
2020-08-27 19:09:17 -0500

    Added Spawners and rounds
    Spawners working
    Rounds can start but dont end
    need to add check for enemy death then deincrement the enemy counter

---
+++ Assets/Scripts/Spawning/Rounds.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rounds : MonoBehaviour
+{
+    [SerializeField] static Spawner[] spawners;
+    private static int roundNum;
+    private static int enemysLeft;
+    private static int newEnemyCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        roundNum = 1;
+        newRound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemysLeft <= 0)
+        {
+            roundNum++;
+            newRound();
+        }
+    }
+
+    void newRound()
+    {
+        calculateEnemyAmount();
+        for (int i = 0; i < spawners.Length; i++)
+        {
+            //add if in range of player here
+
+            spawners[i].setMaxSpawn(newEnemyCount);
+            spawners[i].startNewRound();
+        }
+    }
+
+    public void newEnemy()
+    {
+        enemysLeft++;
+    }
+
+    void calculateEnemyAmount()
+    {
+        if (newEnemyCount > 9)
+        {
+            newEnemyCount++;
+        }
+        else
+        {
+            newEnemyCount = (int)Mathf.Floor((1 / 5) * roundNum + roundNum);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Spawning/Spawner.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Spawner : Rounds
+{
+    [SerializeField] GameObject spawnee;
+    [SerializeField] bool stopSpawning = false;
+    [SerializeField] float spawnTime;
+    [SerializeField] float spawnDelay;
+    private int enemysSpawned;
+    [SerializeField] int maxSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemysSpawned = 0;
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnObject()
+    {
+        base.newEnemy();
+        if (stopSpawning||maxSpawn <= enemysSpawned)
+        {
+            CancelInvoke("SpawnObject");
+        }
+        enemysSpawned++;
+        if(maxSpawn <= enemysSpawned)
+        {
+            stopSpawning = true;
+        }
+        Instantiate(spawnee, transform.position, transform.rotation);
+    }
+    
+    public void startNewRound()
+    {
+        stopSpawning = false;
+        enemysSpawned = 0;
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    //getters
+    
+
+    //setters
+    public void setMaxSpawn(int max)
+    {
+        maxSpawn = max;
+    }
+
+    
+}
