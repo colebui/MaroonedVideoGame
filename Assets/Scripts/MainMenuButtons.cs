b25d254ba85f6989c4cd86e0241b3d6b1eb97f1a
Cameron-T-Cassidy <ccassidy4138@tamu.edu>
2020-08-29 16:44:09 -0500

    Added main menu

--- Assets/Scripts/MainMenuButtons.cs
+++ Assets/Scripts/MainMenuButtons.cs
@@ -1,11 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class mainMenuButtons : MonoBehaviour
-{
-    public void LoadScene(string sceneName)
-    {
-        SceneManager.LoadScene(BrendenTestScene);
-    }
-}
+﻿
