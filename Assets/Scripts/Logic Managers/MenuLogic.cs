908663ed707bbad1437497978705ee6acf408bfd
Cameron-T-Cassidy <ccassidy4138@tamu.edu>
2020-08-31 14:20:15 -0500

    Menu backup

---
+++ Assets/Scripts/Logic Managers/MenuLogic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuLogic : MonoBehaviour
+{
+    [SerializeField] Canvas PrepareCanvas;
+    [SerializeField] Canvas TitleCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Debug.Log("Hello, World"); 
+    }
+    void StartPrepMenu()
+    {
+        TitleCanvas.gameObject.SetActive(false);
+        PrepareCanvas.gameObject.SetActive(true);
+        Time.timeScale = 1;
+        SceneManager.LoadScene("BrendenTestScene");
+    }
+    public void StartButtonClicked()
+    {
+        StartPrepMenu();
+    }
+}
