457874825530664be705b0e6633d894bf276c464
Cameron-T-Cassidy <ccassidy4138@tamu.edu>
2020-09-13 16:38:46 -0500

    cocking sound debug

---
+++ Assets/Scripts/Misc/treasure hunt/chestSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chestSpawner : MonoBehaviour
+{
+    [SerializeField] GameLogic gl; 
+    int chestType;
+    [SerializeField]
+    // 0 = null, 1 = points, 2 = power weapon, 3 = ammo (if needed)
+    void Start()
+    {
+        chestType = 0;
+        //gameObject.SetActive(false);
+    }
+    void collided() {
+        if (chestType == 0)
+            return;
+        if (chestType == 1) { 
+            //give player points
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            gl.addScore(420);
+            chestType = 0;
+            gameObject.SetActive(false);
+        }
+    }
+}
