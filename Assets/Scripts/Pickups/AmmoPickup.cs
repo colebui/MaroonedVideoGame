1fa8109079f0e5a946f75d311fc4050e55bd175c
Brenden Hover <Brenden_hoverXD@live.com>
2020-09-16 17:22:07 -0500

    Basic limited ammo system working

---
+++ Assets/Scripts/Pickups/AmmoPickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This currently only supports ammo for the pistols, which is the only weapon we plan on having limited ammo
+public class AmmoPickup : MonoBehaviour {
+
+    [SerializeField] int ammoValue = 5;
+
+    private Pistol playerPistol;
+
+    // Start is called before the first frame update
+    void Start() {
+        playerPistol = FindObjectOfType<Pistol>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+
+        // If not player, return
+        if(!other.GetComponent<PlayerHealth>()) { return; }
+
+        playerPistol.AddAmmo(ammoValue);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Weapons/Pistol.cs
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -7,15 +7,33 @@ public class Pistol : Weapon {
 
     [SerializeField] TextMeshProUGUI reloadingText;
 
+    [Header("Ammo System")]
+    [SerializeField] int maxAmmo = 20;
+    [SerializeField] int currentAmmo;
+
+    protected override void Start() {
+        base.Start();
+        currentAmmo = maxAmmo;
+    }
+
     protected override void Update() {
         base.Update();
         reloadingText.text = "Reloading " + Mathf.Clamp((timeBetweenAttacks - timeSinceAttacking), 0f, timeBetweenAttacks).ToString("0.00");
     }
 
     public override void Attack() {
+
+        if(currentAmmo <= 0) { return; }
+
         base.Attack();
         Debug.Log("Fired pistol");
         ((HitscanDamage)damageType).ProcessShot();
+
+        currentAmmo--;
+    }
+
+    public void AddAmmo(int ammountToAdd) {
+        currentAmmo = Mathf.Clamp(currentAmmo + ammountToAdd, 0, maxAmmo);
     }
 
     // No start or update, because they don't need to be overridden
