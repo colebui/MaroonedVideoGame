6e0345df4580d6b836e497af906c5c030b0f7cda
unknown <colebui@tamu.edu>
2020-09-21 18:39:31 -0500

    audio zone functionality 1.0

---
+++ Assets/Scripts/MusicController/JungleMusicCounter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JungleMusicCounter : MonoBehaviour
+{
+    public bool inside;
+    public int colliderCount;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip jungleAmbient;
+    [SerializeField] float FADETIME = 5.0f;
+
+    public void Enter(Collider other) {
+            colliderCount += 1;
+            UpdateState();
+    }
+
+    public void Exit(Collider other) {
+            colliderCount -= 1;
+            UpdateState();
+    }
+
+    void UpdateState() {
+        Debug.Log("colliderCount: " + colliderCount);
+        if (colliderCount == 0) {
+            inside = false;
+            StopMusic();
+        }
+        else if (colliderCount > 0 && !audioSource.isPlaying) { 
+            inside = true;
+            PlayMusic();
+        }
+    }
+
+    void PlayMusic() {
+        if (!audioSource.isPlaying) {
+            audioSource.PlayOneShot(jungleAmbient, 1.0f);
+        }
+    }
+
+    void StopMusic() {//cole fix later
+        if (audioSource.isPlaying) {
+            float startVolume = audioSource.volume;
+            while (audioSource.volume > 0) {
+                audioSource.volume -= startVolume * Time.deltaTime / FADETIME;
+            }
+            audioSource.Stop();
+            audioSource.volume = startVolume;
+        }
+    }
+}
