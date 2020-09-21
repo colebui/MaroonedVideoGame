e6dcfe53cb154ae62853500011faae41151b6d52
Brenden Hover <Brenden_hoverXD@live.com>
2020-09-21 17:38:41 -0500

    Some blunderbuss visuals in, moved some folders

--- Assets/Scripts/Weapons/Blunderbuss.cs
+++ Assets/Scripts/Weapons/Blunderbuss.cs
@@ -1,7 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Blunderbuss : PowerWeapon {
+
+    [SerializeField] TextMeshProUGUI CDText;
+
+    protected override void LaunchAttack() {
+        Debug.Log("called");
+    }
+
+    protected override void Update() {
+        base.Update();
+        CDText.text = "Blunderbuss CD: " + Mathf.Clamp((timeBetweenAttacks - timeSinceAttacking), 0f, timeBetweenAttacks).ToString("0.00");
+    }
 
 }
