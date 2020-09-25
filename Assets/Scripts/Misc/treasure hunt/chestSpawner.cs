c01aa5a7cb318b8a38ce31463eae9d98e44eda26
Cameron-T-Cassidy <ccassidy4138@tamu.edu>
2020-09-25 14:15:19 -0500

    Added Treasure hunt
    The ammo chest adds ammo. The power weapons are not added upon flinding their chest; this can be changed in chestSpawner.cs

--- Assets/Scripts/Misc/treasure hunt/chestSpawner.cs
+++ Assets/Scripts/Misc/treasure hunt/chestSpawner.cs
@@ -1,37 +1,103 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Security.Policy;
 using UnityEngine;
 
 public class chestSpawner : MonoBehaviour
 {
-    [SerializeField] GameLogic gl; 
+    [SerializeField] GameLogic gl;
+    [SerializeField] AudioSource foundSound;
+    [SerializeField] AudioClip foundClip;
+    [SerializeField] AudioClip bluderbussUnlocked;
+    [SerializeField] AudioClip speargunUnlocked;
     int chestType;
-    [SerializeField]
-    // 0 = null, 1 = points, 2 = power weapon, 3 = ammo (if needed)
+    // 0 = null, 1 = power weapon 1; 2 = power weapon 2; 3 = points; 4 = ammo
     void Start()
     {
         chestType = 0;
-        //gameObject.SetActive(false);
     }
-    void collided() {
-        if (chestType == 0)
+
+    void changeState(bool io, int type)
+    {
+        chestType = type;
+        Transform chestT = gameObject.transform;
+        if (type == 0)
+        {
+            foreach (Transform child in chestT)
+            {
+                if (!child.name.Contains("Spawner"))
+                    child.gameObject.SetActive(false);
+            }
             return;
-        if (chestType == 1) { 
-            //give player points
+        }
+        gameObject.transform.Find("mapIcon").gameObject.SetActive(io);
+        if (type == 1)
+        {
+            gameObject.transform.Find("speargunChest").gameObject.SetActive(io);
+        }
+        else if (type == 2)
+        {
+            gameObject.transform.Find("blunderbussChest").gameObject.SetActive(io);
+        }
+        else if (type == 3)
+        {
+            gameObject.transform.Find("coinChest").gameObject.SetActive(io);
+        }
+        else if (type == 4) {
+            gameObject.transform.Find("ammoChest").gameObject.SetActive(io);
         }
     }
-    // Update is called once per frame
-    void Update()
+    public void setState(int type)
     {
-        
+        chestType = type;
+        if (type == 0)
+        {
+            changeState(false, type);
+            return;
+        }
+        changeState(true, type);
     }
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.name == "Player")
         {
-            gl.addScore(420);
-            chestType = 0;
-            gameObject.SetActive(false);
+            AudioClip sound = foundClip;
+            if (chestType == 0)
+            {
+                return;
+            }
+            else if (chestType == 1)
+            {
+                sound = speargunUnlocked;
+                Debug.Log("Picked up power weapon 1\n");
+            }
+            else if (chestType == 2)
+            {
+                sound = bluderbussUnlocked;
+                Debug.Log("Picked up power weapon 2\n");
+            }
+            else if (chestType == 3)
+            {
+                Debug.Log("Picked up points\n");
+                gl.addScore(420);
+            }
+            else if (chestType == 4)
+            {
+                FindObjectOfType<Pistol>().AddAmmo(20);
+                Debug.Log("Picked up ammo \n");
+            }
+            else
+            {
+                Debug.Log("ERROR chestSpawner:onTrigger: Invalid chestType\n");
+            }
+            if (foundSound != null)
+            {
+                foundSound.PlayOneShot(sound, 4f);
+            }
+            FindObjectOfType<TreasureHuntMain>().chestFound();
+            setState(0);
         }
     }
 }
--- Assets/Scripts/Spawning/Rounds.cs
+++ Assets/Scripts/Spawning/Rounds.cs
@@ -57,6 +57,7 @@ public class Rounds : MonoBehaviour
 
     void newRound()
     {
+        FindObjectOfType<TreasureHuntMain>().roll();
         FindObjectOfType<GameLogic>().addScore(roundPayout);
         roundNum++;
         Debug.Log("Round number " + roundNum);
