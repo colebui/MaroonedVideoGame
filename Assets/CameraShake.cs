c1856504918b5569c2ac07fa81568d12e93b9305
unknown <colebui@tamu.edu>
2020-10-21 15:04:54 -0500

    fixed camera shaking bug

--- Assets/CameraShake.cs
+++ Assets/CameraShake.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    
+    Vector3 originalPos;
+    void Start() {
+        originalPos = transform.localPosition;
+    }
     public IEnumerator Shake (float duration, float magnitude) {
         Debug.Log("shake");
-        Vector3 originalPos = transform.localPosition;
+        
         float elapsed = 0.0f;
 
         while (elapsed < duration) {
