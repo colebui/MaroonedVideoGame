0ba69f01a1cf56fbafe138cfd0ecd7c282756df7
Cameron-T-Cassidy <ccassidy4138@tamu.edu>
2020-11-11 21:00:09 -0600

    Added buy station, collider needs to be hooked up to UI.

---
+++ Assets/Scripts/Pickups/buyStationCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buyStationCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        //if (col.gameObject.name == "Player")
+        //{
+        //}
+    }
+}
