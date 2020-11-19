4bfe5c9a06382775a46c0540e35f659b7f6ad599
Brenden Hover <Brenden_hoverXD@live.com>
2020-11-18 18:37:19 -0600

    Fixing bugs that came out of nowhere causing the game to run like ass

--- Assets/Scripts/Enemy AI/EnemyController.cs
+++ Assets/Scripts/Enemy AI/EnemyController.cs
@@ -46,7 +46,6 @@ public class EnemyController : MonoBehaviour
 
         animator.SetFloat(spawnSpeedParameter, UnityEngine.Random.Range(minSpawnAnimationSpeed, maxSpawnAnimationSpeed));
 
-        Debug.Log("difficultyModifier is " + LoadingOptions.difficultyModifier);
         MIN_SPEED *= LoadingOptions.difficultyModifier;
         MAX_SPEED *= LoadingOptions.difficultyModifier;
     }
